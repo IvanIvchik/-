683f8d92b691df103667737dcfe96ed651a42d8e
IvanIvchik <ivanivchik11@gmail.com>
2021-05-31 21:32:54 +0300

    My layout

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿function rollUpChat() {
+    document.getElementById("chatBot").style.display = "none";
+    document.getElementById("stringChatBot").style.display = "block";
+}
+
+function openChat() {
+    document.getElementById("chatBot").style.display = "block";
+    document.getElementById("stringChatBot").style.display = "none";
+}
+
+let timerId = setTimeout(() => openChat(), 10000);
